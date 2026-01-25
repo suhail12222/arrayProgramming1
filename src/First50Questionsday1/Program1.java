81e89e1da3cb8df98f506c31c6e383d554c899b7
Suhail <muhmmadasifmalik593@gmail.com>
2026-01-25 16:19:49 +0530

    Updated array program

--- src/First50Questionsday1/Program1.java
+++ src/First50Questionsday1/Program1.java
@@ -1,7 +1,22 @@
 package First50Questionsday1;
-
+//Todo  Print biggest element , smallest elements and their difference from the given array.
 public class Program1 {
     public static void main(String[] args) {
+int []a={1,2,3,4,5,1,2};
+printBiggestAndSmallest(a);
+    }
+    public static void printBiggestAndSmallest(int []a){
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for (int i=0;i<a.length;i++){
+            if (a[i]<min){
+                min=a[i];
+            } else if (a[i]>max) {
+                max=a[i];
 
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
     }
 }
