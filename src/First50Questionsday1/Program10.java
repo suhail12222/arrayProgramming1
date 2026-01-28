2fa9d07d301672535304dbd34200cf4f08356ca8
Suhail <muhmmadasifmalik593@gmail.com>
2026-01-28 12:24:20 +0530

    adding program to find the even numbers using strams

--- src/First50Questionsday1/Program10.java
+++ src/First50Questionsday1/Program10.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class Program10 {
     public static void main(String[] args) {
 int []a={1,2,3,1,21,8,2,1,2,2,2,223,4,4,4};
-        System.out.println(countPrimes(a));
+        System.out.println(countEven(a));
     }
-    public static int countPrimes(int []a){
+    public static int countEven(int []a){
         long count= Arrays.stream(a).filter(i->i%2==0).count();
         return (int)count;
     }
---
+++ src/First50Questionsday1/Program11.java
@@ -0,0 +1,8 @@
+package First50Questionsday1;
+
+public class Program11 {
+    public static void main(String[] args) {
+
+    }
+    public static int
+}
