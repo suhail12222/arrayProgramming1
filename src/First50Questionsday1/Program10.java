921063aaba62f6c39a5fe6e45385108b31571c8a
Suhail <muhmmadasifmalik593@gmail.com>
2026-01-28 12:22:51 +0530

    findng the even number in an array

---
+++ src/First50Questionsday1/Program10.java
@@ -0,0 +1,14 @@
+package First50Questionsday1;
+
+import java.util.Arrays;
+
+public class Program10 {
+    public static void main(String[] args) {
+int []a={1,2,3,1,21,8,2,1,2,2,2,223,4,4,4};
+        System.out.println(countPrimes(a));
+    }
+    public static int countPrimes(int []a){
+        long count= Arrays.stream(a).filter(i->i%2==0).count();
+        return (int)count;
+    }
+}
